7708ba0aa154958e8ae273a4e41c6d349ba06740
Anthony Sottile <asottile@umich.edu>
2015-12-01 12:40:05 -0800

    Add a fun yaml wat

---
+++ python/yaml_lols.py
@@ -0,0 +1,17 @@
+"""This one requires pyyaml.  pyyaml is pretty not-safe by default.
+Takeaway: use yaml.safe_load
+"""
+import yaml
+
+
+def main():
+    print(
+        '>>> yaml.load('
+        '"!!python/object/apply:os.system\\nargs: [\'echo hi\']"'
+        ')'
+    )
+    print(yaml.load("!!python/object/apply:os.system\nargs: ['echo hi']"))
+
+
+if __name__ == '__main__':
+    exit(main())
