7d920a982c6f6f6d0284eb9047a9dbcf18884618
Anthony Sottile <asottile@umich.edu>
2016-04-05 09:16:53 -0700

    Add wat about True == not False

---
+++ python/not_syntax_error.py
@@ -0,0 +1,18 @@
+import traceback
+
+
+def eval_and_print(s):
+    try:
+        print('>>> {}'.format(s))
+        print(eval(s))
+    except Exception:
+        traceback.print_exc()
+
+
+def main():
+    eval_and_print('not False == True')
+    eval_and_print('True == not False')
+
+
+if __name__ == '__main__':
+    exit(main())
