cd84c2ac5dffc4912b5ed193abd82f9c72c7fd04
Anthony Sottile <asottile@umich.edu>
2016-05-27 14:52:40 -0700

    Add the thing that javascript gets a bad rep for to python

---
+++ python/closures.py
@@ -0,0 +1,10 @@
+x = (1, 2, 3)
+
+for (replace_key, replace_value) in ((2, 4), (3, 5)):
+    x = (replace_value if replace_key == p else p for p in x)
+
+print(tuple(x))
+
+OUTPUT = """\
+(1, 2, 5)
+"""
